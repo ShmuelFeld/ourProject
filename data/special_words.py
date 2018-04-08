761d4f827b375d86d0a555cdd1aaae0274792ae5
shmuelfeld <sfeld305@gmail.com>
2018-04-08 13:02:32 +0300

    fullCode

---
+++ data/special_words.py
@@ -0,0 +1,13 @@
+"""Special word constants.
+NOTE: The ids of the EOS and UNK constants should not be modified. It is assumed
+that these always occupy tjhe first two ids.
+"""
+
+# End of sentence.
+EOS = "<eos>"
+EOS_ID = 0
+
+# Unknown.
+UNK = "<unk>"
+UNK_ID = 1
+print "kjddfszsd;lzkc;kldc;k"
