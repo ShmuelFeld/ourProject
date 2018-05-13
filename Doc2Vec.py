77083701db34ac47a1cb7b71641ea5d1f645a84e
shmuelfeld <sfeld305@gmail.com>
2018-05-13 18:30:02 +0300

    communication works

--- Doc2Vec.py
+++ Doc2Vec.py
@@ -1,45 +1,54 @@
+from textblob import TextBlob as tb
 
+import sentence
 from TfidfVocabCreator import Tfidf_Creator as TFIDF
 import configuration
 import encoder_manager
 import numpy as np
-
+import document as doc
 
 # Set paths to the model.
 VOCAB_FILE = "./data/vocab.txt"
 EMBEDDING_MATRIX_FILE = 'use_trained_model'
 CHECKPOINT_PATH = "./model/train/model.ckpt-22638"
-TEXT_FILE = "/home/shmuelfeld/Desktop/inputFiles_Heb/2505_20171206_124724.txt"
+TEXT_FILE = "/home/shmuelfeld/Desktop/inputFiles_Heb/*.txt"
 
 data = []
 tfidf = TFIDF()
-tfidf_dict = tfidf.create_TFIDF_dictionary()
+tfidf_dict = tfidf.get_tfidf_dic(TEXT_FILE)
 encoder = encoder_manager.EncoderManager()
 encoder.load_model(configuration.model_config(),
                    vocabulary_file=VOCAB_FILE,
                    embedding_matrix_file=EMBEDDING_MATRIX_FILE,
                    checkpoint_path=CHECKPOINT_PATH)
 
+
 def sentence_to_vec(sentence):
     sen = list()
     sen.append(sentence)
     return encoder.encode(sen)
 
-def doc2vec(path):
-    with open(path, 'r') as fi:
-        vec = sens2vec(fi.readlines())
-    fi.close()
-    return vec
 
-def sens2vec(lst):
-    vecs = encoder.encode(lst)
-    doc_val = tfidf.get_document_value(lst)
-    tfidfs = tfidf.get_vector_values(lst)
-    mult_by_scalar = []
-    for i in range(0, len(vecs)):
-        mult_by_scalar.append(np.dot(vecs[i]/doc_val, tfidfs[i]))
-    the_vec = sum(mult_by_scalar)
+def sens2vec(list_of_sentences, total_tfidf):
+    multed = []
+    for sent in list_of_sentences:
+        if total_tfidf == 0:
+            total_tfidf += 1
+        multed.append(np.dot(sent.tfidf_val/total_tfidf, sent.vector))
+    the_vec = sum(multed)
     return the_vec
+
+def create_doc_object(list_of_sentences):
+    sentences = []
+    total_tfidf = 0
+    for sent in list_of_sentences:
+        words = tb(sent).words
+        tfidf_dic = tfidf.generate_dictionary_for_specific_words(words)
+        tfidf_val = tfidf.get_sentence_value(sent)
+        vector = sentence_to_vec(sent)
+        sentences.append(sentence.sentence(sent, tfidf_dic, vector[0],tfidf_val))
+        total_tfidf += tfidf_val
+    return doc.documant(sentences, total_tfidf, sens2vec(sentences, total_tfidf))
 
 
 
--- TfidfVocabCreator.py
+++ TfidfVocabCreator.py
@@ -1,30 +1,40 @@
 import math
 from textblob import TextBlob as tb
 import tensorflow as tens
-import json
+from pathlib import Path
+
+import Doc2Vec
+import sentence
+from document import documant
+
 tfs = {}
+PATH_TO_DIC = './tfidfvocab.txt'
+
+# create tfidf dictionary
 class Tfidf_Creator:
     def __init__(self, input_path = "/home/shmuelfeld/Desktop/inputFiles_Heb/*.txt"):
         self.input_files = input_path
-
+        self.path_to_dic = PATH_TO_DIC
+    # return the #word divide by number of words in file
     def tf(self, word, blob):
         return blob.words.count(word) / len(blob.words)
-
+    # how many files contains thw word
     def n_containing(self, word, bloblist):
         return sum(1 for blob in bloblist if word in blob)
-
+    # computes "inverse document frequency" which measures how common a word is
+    #  among all documents in bloblist
     def idf(self, word, bloblist):
         return math.log(len(bloblist) / (1 + self.n_containing(word, bloblist)))
-
+    # compute tfidf value for word
     def tfidf(self, word, blob, bloblist):
         return self.tf(word, blob) * self.idf(word, bloblist)
-
+    # create string from list of strings for the textblob
     def listToString(self, list):
         ret = ""
         for sent in list:
             ret += sent
         return ret
-
+    # create counter dictionary. how many times each word appears in the files.
     def create_counter(self):
         pattern = self.input_files
         input_files = []
@@ -42,7 +52,6 @@ class Tfidf_Creator:
         for word in se:
             num = 0
             dic[word] = num
-
         for file in input_files:
             with open(file, 'r') as fi:
                 se = tb(self.listToString(fi.readlines()))
@@ -57,9 +66,24 @@ class Tfidf_Creator:
                 st += '\n'
                 file.write(st)
         file.close()
+    # read tfidf dic from file
+    def read_dic_from_file(self):
+        with open(self.path_to_dic, 'r') as fi:
+            for line in fi.readlines():
+                li = line.replace('\n','')
+                tfs[li.split(':')[0]] = float(li.split(':')[1])
+        fi.close()
+        return tfs
 
-    def create_TFIDF_dictionary(self):
-        pattern = self.input_files
+    # return tfidf dictionary, if the file already exist, so read from the file, else create it
+    def get_tfidf_dic(self, input_files_path):
+        if Path(self.path_to_dic).is_file():
+            return self.read_dic_from_file()
+        return self.create_TFIDF_dictionary(input_files_path)
+
+    # create tfidf dictionary file
+    def create_TFIDF_dictionary(self,input_files_path):
+        pattern = input_files_path
         input_files = []
         match = tens.gfile.Glob(pattern)
         if not match:
@@ -70,20 +94,29 @@ class Tfidf_Creator:
             with open(file, 'r') as fi:
                 bloblist.append(tb(self.listToString(fi.readlines())))
             fi.close()
+        vocab = open('tfidfvocab.txt', 'w+')
         for i, blob in enumerate(bloblist):
-            print("Top words in document {}".format(i + 1))
             scores = {word: self.tfidf(word, blob, bloblist) for word in blob.words}
             for score in scores:
                 tfs[score] = scores[score]
                 # sorted_words = sorted(scores.items(), key=lambda x: x[1], reverse=True)
                 # for word, score in sorted_words[:10]:
                 #     print("Word: {}, TF-IDF: {}".format(word, round(score, 5)))
+            for key, value in tfs.items():
+                st = ''
+                st += key
+                st += ':'
+                st += str(value)
+                st += '\n'
+                vocab.write(st)
         return tfs
 
+    # returns the tfidf value of a given sentence
     def get_sentence_value(self, sentence):
         sentsum = 0
         for word in tb(sentence).words:
-            sentsum += tfs[word]
+            if word in tfs:
+                sentsum += tfs[word]
         return sentsum
     def get_document_value(self, lst):
         docsum = 0
@@ -95,3 +128,14 @@ class Tfidf_Creator:
         for sen in lst:
             tfidfvec.append(self.get_sentence_value(sen))
         return tfidfvec
+
+    def generate_dictionary_for_specific_words(self, words):
+        tf_words = {}
+        for word in words:
+            if word in tfs:
+                tf_words[word] = tfs[word]
+            else:
+                tf_words[word] = 0
+        return tf_words
+
+
